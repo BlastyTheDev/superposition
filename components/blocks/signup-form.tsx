c5b3264167f037d3c36b6012f662132bf82a04fd
Blasty <rmzha2234@gmail.com>
2026-06-04 19:18:59 +1000

    Implement react-hook-form on signup

--- components/blocks/signup-form.tsx
+++ components/blocks/signup-form.tsx
@@ -1,24 +1,49 @@
+"use client"
+
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
 import {
   Field,
   FieldDescription,
+  FieldError,
   FieldGroup,
   FieldLabel,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
 import Link from "next/link"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Controller, useForm } from "react-hook-form"
+import * as z from "zod"
+
+const signupSchema = z.object({
+  code: z.string().length(14, "Code must be exactly 14 characters."),
+  email: z.email("Invalid email."),
+  password: z.string().min(8, "Password must be at least 8 characters.")
+})
 
 export function SignupForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const form = useForm<z.infer<typeof signupSchema>>({
+    resolver: zodResolver(signupSchema),
+    defaultValues: {
+      code: "",
+      email: "",
+      password: ""
+    }
+  })
+
+  function onSubmit(data: z.infer<typeof signupSchema>) {
+  }
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -26,40 +51,76 @@ export function SignupForm({
           <CardTitle className="text-xl">Sign up for Superposition</CardTitle>
         </CardHeader>
         <CardContent>
-          <form>
+          <form id="form-signup" onSubmit={form.handleSubmit(onSubmit)}>
             <FieldGroup>
-              <Field>
-                <FieldLabel htmlFor="code">Access Code</FieldLabel>
-                <Input
-                  id="code"
-                  placeholder="A1B2-C3D4-E5F6"
-                  required
-                />
-              </Field>
-              <Field>
-                <FieldLabel htmlFor="email">Email</FieldLabel>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="you@example.com"
-                  required
-                />
-              </Field>
-              <Field>
-                <div className="flex items-center">
-                  <FieldLabel htmlFor="password">Password</FieldLabel>
-                </div>
-                <Input id="password" type="password" required />
-              </Field>
-              <Field>
-                <Button type="submit">Login</Button>
-                <FieldDescription className="text-center">
-                  Already have an account? <Link href="/login">Log in</Link>
-                </FieldDescription>
-              </Field>
+              <Controller
+                name="code"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="code">Access Code</FieldLabel>
+                    <Input
+                      {...field}
+                      id="code"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="A1B2-C3D4-E5F6"
+                      required
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )} />
+              <Controller
+                name="email"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="email">Email</FieldLabel>
+                    <Input
+                      {...field}
+                      id="email"
+                      aria-invalid={fieldState.invalid}
+                      type="email"
+                      placeholder="you@example.com"
+                      required
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )} />
+              <Controller
+                name="password"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <div className="flex items-center">
+                      <FieldLabel htmlFor="password">Password</FieldLabel>
+                    </div>
+                    <Input
+                      {...field}
+                      id="password"
+                      aria-invalid={fieldState.invalid}
+                      type="password"
+                      required
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )} />
             </FieldGroup>
           </form>
         </CardContent>
+        <CardFooter>
+          <Field>
+            <Button type="submit" form="form-signup">Sign up</Button>
+            <FieldDescription className="text-center">
+              Already have an account? <Link href="/login">Log in</Link>
+            </FieldDescription>
+          </Field>
+        </CardFooter>
       </Card>
     </div>
   )
